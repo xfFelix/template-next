80c354516c37e3473a6eb2b62327d5c0c26280ac
felix <xffelix_fei@163.com>
2025-09-16 11:52:29 +0800

    feat: 删除next lint,使用eslint进行校验

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,9 +1,9 @@
 import path from 'path';
 
 const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
+  `eslint --fix ${filenames
     .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`;
+    .join(' ')}`;
 
 const buildStylelintCommand = (filenames) =>
   `stylelint --fix ${filenames
